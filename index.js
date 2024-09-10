d295a8659c43654c72a98194e0b2d220be636701
Aaron Cordeau <akcordeau@gmail.com>
2024-09-10 12:07:51 -0700

    Add login/register plan

--- index.js
+++ index.js
@@ -3,3 +3,41 @@ Title: Foundations Project
 Author: Aaron Cordeau
 Version: 1.0
 */
+
+// Entity Plan
+
+// EMPLOYEE
+/*
+
+    Login
+    Add reimbursement request
+    View updated status
+
+ */
+
+// Finance Manager
+/*
+
+    Login
+    View pending request
+    Approve/Deny
+
+*/
+
+const login = ({ username, password }) => {
+  // This is where we do some verification with
+  // the database to check for a few things
+  // Checking for successful login
+  // Checking for failed login (invalid credentials)
+  // Checking for failed login (invalid user)
+  // Checking for role
+};
+
+const register = ({ username, password }) => {
+  // This is where we do some verification with
+  // the database to check for a few things
+  // Checking for successful registration
+  // Checking for failed registration (user already exists)
+  // Checking for failed registration (missing username or password)
+  // Gives employee default role
+};
