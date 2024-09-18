39302d91e7a8e958e91ccd2ac2b6fd2d0b60c985
Aaron Cordeau <akcordeau@gmail.com>
2024-09-18 14:15:01 -0700

    Update program to use DynamoDB for Data Persistence

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 const UserDao = require("../dao/userDAO");
 
 class UserController {
-  static register(req, res) {
+  static async register(req, res) {
     const { username, password } = req.body;
     if (!username || !password) {
       return res.status(400).json({
@@ -9,20 +9,34 @@ class UserController {
         message: "Username and password are required",
       });
     }
-    const result = UserDao.registerUser(username, password);
-    return res.status(result.success ? 201 : 400).json(result);
+
+    try {
+      const result = await UserDao.registerUser(username, password);
+      return res.status(result.success ? 201 : 400).json(result);
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Error registering user" });
+    }
   }
 
-  static login(req, res) {
-    const { username, password, role } = req.body;
+  static async login(req, res) {
+    const { username, password } = req.body;
     if (!username || !password) {
       return res.status(400).json({
         success: false,
         message: "Username and password are required",
       });
     }
-    const result = UserDao.loginUser(username, password, role);
-    return res.status(result.success ? 200 : 400).json(result);
+
+    try {
+      const result = await UserDao.loginUser(username, password);
+      return res.status(result.success ? 200 : 401).json(result);
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Error during login" });
+    }
   }
 }
 
--- src/dao/userDAO.js
+++ src/dao/userDAO.js
@@ -1,46 +1,89 @@
 const fs = require("fs");
 const path = require("path");
 const User = require("../models/userModel");
+// const dynamodb = require("../data/dynamoClient");
+const uuid = require("uuid");
 
-const usersFilePath = path.join(__dirname, "../data/employees.json");
+const {
+  DynamoDBClient,
+  QueryCommand,
+  ScanCommand,
+} = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  UpdateCommand,
+  DeleteCommand,
+} = require("@aws-sdk/lib-dynamodb");
+
+const client = new DynamoDBClient({ region: "us-east-1" });
+
+const documentClient = DynamoDBDocumentClient.from(client);
+
+const TableName = "Employees";
 
 class UserDao {
-  static getAllUsers() {
-    if (fs.existsSync(usersFilePath)) {
-      const users = fs.readFileSync(usersFilePath);
-      return JSON.parse(users);
+  static async getAllUsers() {
+    const command = {
+      TableName,
+    };
+
+    try {
+      const data = await dynamodb.scan(command);
+      return data.Items || [];
+    } catch (err) {
+      console.error(err);
     }
-    return [];
   }
 
-  static findUserByUsername(username) {
-    const users = this.getAllUsers();
-    return users.find((user) => user.username === username);
+  static async findUserByUsername(username) {
+    const command = new GetCommand({
+      TableName,
+      Key: { username },
+    });
+
+    try {
+      const data = await documentClient.send(command);
+      return data.Item || null;
+    } catch (err) {
+      console.error(err);
+    }
   }
 
-  static saveAllUsers(users) {
-    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
-  }
+  static async registerUser(username, password, role = "employee") {
+    const searchUser = await this.findUserByUsername(username);
 
-  static registerUser(username, password, role = "employee") {
-    const users = this.getAllUsers();
-    if (this.findUserByUsername(username)) {
+    if (searchUser) {
       return { success: false, message: "Username is already taken" };
     }
+
     const newUser = new User(username, password, role);
-    users.push(newUser);
-    this.saveAllUsers(users);
-    return { success: true, message: "Registration successful" };
+
+    const command = new PutCommand({
+      TableName,
+      Item: newUser,
+    });
+
+    try {
+      await documentClient.send(command);
+      return { success: true, message: "Registration successful" };
+    } catch (err) {
+      console.error(err);
+    }
   }
 
-  static loginUser(username, password, role) {
-    const currentUser = this.findUserByUsername(username);
+  static async loginUser(username, password) {
+    const currentUser = await this.findUserByUsername(username);
+
     if (!currentUser) {
       return { success: false, message: "User not found" };
     }
+
     if (currentUser.password !== password) {
       return { success: false, message: "Invalid password" };
     }
+
     return { success: true, message: "Login successful", currentUser };
   }
 }
